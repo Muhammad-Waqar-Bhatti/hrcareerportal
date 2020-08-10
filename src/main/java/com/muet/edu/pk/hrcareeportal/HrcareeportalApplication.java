a09d5124d67f39d80afc41c677e9b111eafbe64f
Muhammad Waqar <mohammadwaqar401@gmail.com>
2020-08-10 22:35:44 +0500

    Initial Commit

---
+++ src/main/java/com/muet/edu/pk/hrcareeportal/HrcareeportalApplication.java
@@ -0,0 +1,13 @@
+package com.muet.edu.pk.hrcareeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrcareeportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrcareeportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muet/edu/pk/hrcareeportal/HrcareeportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muet.edu.pk.hrcareeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcareeportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
